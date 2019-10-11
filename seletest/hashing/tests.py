9dc29f545403a9819a6c63b99291b14039fd4a94
Liberbell <liberbell@gmail.com>
2019-10-11 12:25:42 +0900

    tests2

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from selenium import webdriver
 
 # Create your tests here.
+browser = webdriver.Safari()
+browser.get('http://localhost:8000')
+
+assert browser.page_source.find('install')
