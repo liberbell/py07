1f6aa73e5a815f3015a20f3103fc3b2214f78ef7
Liberbell <liberbell@gmail.com>
2019-10-11 12:30:39 +0900

    tests4

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -2,7 +2,13 @@ from django.test import TestCase
 from selenium import webdriver
 
 # Create your tests here.
-browser = webdriver.Safari()
+class FunctionalTestCase(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Safari()
+
+    def tearDown(self):
+        self.browser.quit()
+
 browser.get('http://localhost:8000')
 
 assert browser.page_source.find('install')
