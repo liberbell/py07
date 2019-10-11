2fe7a4609c41cb26b33036be53a78c3fa583b711
Liberbell <liberbell@gmail.com>
2019-10-11 12:36:16 +0900

    tests6

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -9,7 +9,7 @@ class FunctionalTestCase(TestCase):
     def test_there_is_homepage(self):
         browser.get('http://localhost:8000')
         # assert browser.page_source.find('install')
-        self.assertIn('install')
+        self.assertIn('install', self.browser.page_source)
 
     def tearDown(self):
         self.browser.quit()
