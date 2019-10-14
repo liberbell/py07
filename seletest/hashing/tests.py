2ed1f139cdf865d010d2f4ec9eb99abb466fad7f
Liberbell <liberbell@gmail.com>
2019-10-14 14:13:59 +0900

    tests4

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -37,4 +37,4 @@ class UnitTestCase(TestCase):
 
     def test_hash_func_works(self):
         text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
-        self.assertEqual('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', hash_text)
+        self.assertEqual('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', text_hash)
