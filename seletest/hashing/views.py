38235d9b44d47122835e5b20077d655489a8bdec
Liberbell <liberbell@gmail.com>
2019-10-15 07:47:55 +0900

    views3

--- seletest/hashing/views.py
+++ seletest/hashing/views.py
@@ -5,5 +5,10 @@ import hashlib
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        filled_form = HashForm(request.POST)
+        if filled_form.is_valid():
+            text = filled_form.cleaned_data('text')
+            text_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
     form = HashForm()
     return render(request, 'hashing/home.html', {'form':form})
