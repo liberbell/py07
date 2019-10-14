81a0fc454d47d5b2268526b1044d82665c82074a
Liberbell <liberbell@gmail.com>
2019-10-15 07:57:33 +0900

    tests10

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -58,4 +58,4 @@ class UnitTestCase(TestCase):
     def test_viewing(self):
         hash = self.saveHash()
         response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
-        self.asserContains(response, 'hello')
+        self.assertContains(response, 'hello')
