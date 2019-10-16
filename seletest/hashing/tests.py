2d03310d3d3a566963c44c61fa380ba8d3fd3afd
Liberbell <liberbell@gmail.com>
2019-10-17 07:41:10 +0900

    tests1

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -4,27 +4,27 @@ from .forms import HashForm
 import hashlib
 from .models import Hash
 
-# Create your tests here.
-# class FunctionalTestCase(TestCase):
-#     def setUp(self):
-#         self.browser = webdriver.Safari()
-#
-#     def test_there_is_homepage(self):
-#         self.browser.get('http://localhost:8000')
-#         # assert browser.page_source.find('install')
-#         # self.assertIn('install', self.browser.page_source)
-#         self.assertIn('Enter hash here', self.browser.page_source)
-#
-#     def tesh_hash_of_hello(self):
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text')
-#         test.send_keys('hello')
-#         self.browser.find_element_by_name('submit').click()
-#         self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
-#
-#
-#     def tearDown(self):
-#         self.browser.quit()
+Create your tests here.
+class FunctionalTestCase(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Safari()
+
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        # assert browser.page_source.find('install')
+        # self.assertIn('install', self.browser.page_source)
+        self.assertIn('Enter hash here', self.browser.page_source)
+
+    def tesh_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        test.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+
+
+    def tearDown(self):
+        self.browser.quit()
 
 class UnitTestCase(TestCase):
 
