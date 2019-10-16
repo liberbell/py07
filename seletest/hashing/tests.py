4eb834bf13565366d33103502c0133fe3c432ed0
Liberbell <liberbell@gmail.com>
2019-10-17 07:41:19 +0900

    tests2

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -22,7 +22,6 @@ class FunctionalTestCase(TestCase):
         self.browser.find_element_by_name('submit').click()
         self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
 
-
     def tearDown(self):
         self.browser.quit()
 
