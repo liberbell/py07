cb6f52d07ff7d896d073e6d11067bee2d5a5c2f5
Liberbell <liberbell@gmail.com>
2019-10-17 07:55:17 +0900

    tests8

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ValidationError
 # Create your tests here.
 class FunctionalTestCase(TestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Safari()
 
     def test_there_is_homepage(self):
         self.browser.get('http://localhost:8000')
@@ -21,7 +21,13 @@ class FunctionalTestCase(TestCase):
         text = self.browser.find_element_by_id('id_text')
         test.send_keys('hello')
         self.browser.find_element_by_name('submit').click()
-        self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+
+    def test_hash_ajax(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        test.send_keys('hello')
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
 
     def tearDown(self):
         self.browser.quit()
