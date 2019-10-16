7145cf13dd6fdbdcee2b75d49ae2d1423e7be7aa
Liberbell <liberbell@gmail.com>
2019-10-17 07:49:56 +0900

    tests6

--- seletest/hashing/tests.py
+++ seletest/hashing/tests.py
@@ -59,6 +59,7 @@ class UnitTestCase(TestCase):
 
     def test_bad_data(self):
         def badHash():
+            hash = Hash()
             hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824aklljfssfdsas'
             hash.full_clean()
         self.assertRaises(ValidationError, badHash)
